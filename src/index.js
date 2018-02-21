db78865ef89cf88642c47f252f5278c09f25e908
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-02-21 18:31:44 +0600

    Functional Components

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-        return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -27,8 +18,16 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares});
+    }
+
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}/>;
     }
 
     render() {
@@ -62,7 +61,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -76,6 +75,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
