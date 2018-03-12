66b932c9e3a240e3d4f9c102f1326be607000a86
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-03-12 21:54:10 +0600

    Create Store & Reducer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// ========================================
+/* Redux Tutorial Start */
+import {createStore} from 'redux';
+
+const reducer = () => {
+    return 'STATE';
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+/* Redux Tutorial End */
 
 ReactDOM.render(
     <App/>,
