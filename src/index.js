3b7ae20b2466faa2cdd62a04ae97756439f87a40
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-03-12 22:01:27 +0600

    Action Dispatcher

--- src/index.js
+++ src/index.js
@@ -8,12 +8,30 @@ import registerServiceWorker from './registerServiceWorker';
 /* Redux Tutorial Start */
 import {createStore} from 'redux';
 
-const reducer = () => {
-    return 'STATE';
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "CHANGE_STATE":
+            //TODO May be API Call is here
+            return action.payload.newState;
+            break;
+        default:
+            return 'STATE';
+            break;
+    }
 };
 
 const store = createStore(reducer);
 
+console.log(store.getState());
+
+const action = {
+  type: 'CHANGE_STATE',
+  payload: {
+      newState: 'Hello World New State'
+  }
+};
+
+store.dispatch(action);
 console.log(store.getState());
 
 /* Redux Tutorial End */
