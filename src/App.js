290a0d874dfe3bdfda0323f77425a0539787c0ff
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-03-13 09:32:18 +0600

    mergeProps = propsFromState + propsFromDispatch + ownProps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {updateUser} from './actions/user';
 
@@ -11,17 +12,17 @@ class App extends Component {
     }
 
     onUpdateUser(e) {
-      this.props.onUpdateUser(e.target.value);
+        this.props.onUpdateUser(e.target.value);
     }
 
     render() {
-        console.log('Hello: ', this.props);
+        // console.log('Hello: ', this.props);
 
         return (
             <div>
                 <h1>{this.props.title}</h1>
                 <p>{this.props.user}</p>
-                <input type="text" className="form-control" onChange={this.onUpdateUser} />
+                <input type="text" className="form-control" onChange={this.onUpdateUser}/>
                 <button className="btn btn-default"
                         onClick={this.onUpdateUser}>Click me
                 </button>
@@ -38,11 +39,21 @@ const mapStateToProps = (state, props) => {
     };
 };
 
-const mapActionsToProps = {
-    onUpdateUser: updateUser
+const mapActionsToProps = (dispatch, props) => {
+    // console.log('Passed Props: ', props);
+
+    return bindActionCreators({
+        onUpdateUser: updateUser
+    }, dispatch);
+};
+
+const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+    console.log(propsFromState, propsFromDispatch, ownProps);
+    return {};
 };
 
 export default connect(
     mapStateToProps,
     mapActionsToProps,
+    mergeProps
 )(App);
