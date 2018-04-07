0ccd71e243cb11b08371a3c7b23c43b4b3b5a938
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-04-07 19:31:11 +0600

    store & provider initialize

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # React and Redux Kickstarter
 
 * create-react-app <app-name>
-* npm install --save redux react-redux
+* npm install --save redux react-redux redux-thunk
 
 Selector Library for Redux
 https://github.com/reactjs/reselect
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
 import './styles/index.css';
 import './styles/sass/stylesheets/styles.css';
 
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
+const app_store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={app_store}>
+        <Main/>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
 
 /**
