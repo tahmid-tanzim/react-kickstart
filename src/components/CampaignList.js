4ffba19ca754a5b0d0be42f02fed90a1ad9cd735
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-04-22 00:31:22 +0600

    First Test Case

--- src/components/CampaignList.js
+++ src/components/CampaignList.js
@@ -102,7 +102,7 @@ class CampaignList extends Component {
         .catch(error => console.error(error));
     }
 
-    componentWillMount() {
+    componentDidMount() {
         // this.setState({
         //     isLoading: true
         // });
