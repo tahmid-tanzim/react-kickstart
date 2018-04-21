14243b9719019ea6f3bf2d62f00541940385d3da
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-04-22 00:47:08 +0600

    Snapshot Tests with Jest

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ Hot reload not working https://github.com/reactjs/react-redux/issues/502
 ### How to achieve Immutability
 1. Array: https://egghead.io/lessons/javascript-redux-avoiding-array-mutations-with-concat-slice-and-spread
 2. Objects: https://egghead.io/lessons/javascript-redux-avoiding-object-mutations-with-object-assign-and-spread
+
+### Snapshot Tests with Jest
+npm install --save-dev react-test-renderer
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Main from './Main';
+import Alerts from './components/Alerts';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Main />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Main', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Main/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('has a valid snapshot', () => {
+        const component = renderer.create(
+            <Main />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
+
+describe('Alerts', () => {
+
+    const props = {
+        type: 'success',
+        value: 'Campaign added successfully'
+    };
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Alerts {...props} />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('has a valid snapshot', () => {
+        const component = renderer.create(
+            <Alerts {...props} />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
 });
